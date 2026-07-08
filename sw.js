459d62de06b1163ad52205f12d5291f833e5061d
Chihoon Lee <chihoon.lee121@gmail.com>
2026-07-08 23:15:25 +0200

    Force cache bypass

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
